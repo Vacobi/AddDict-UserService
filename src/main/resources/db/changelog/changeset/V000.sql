3e6aa319efb01f619f4445b88e252b27c681c8d5
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-13 01:01:36 +0300

    refactor: refactored existing db migrations

--- src/main/resources/db/changelog/changeset/V000.sql
+++ src/main/resources/db/changelog/changeset/V000.sql
@@ -1,6 +1,8 @@
 create table if not exists "user"
 (
-    id bigint primary key generated always as identity unique,
-    login varchar(32) not null unique,
-    password varchar(64) not null
+    id                     bigint primary key generated always as identity unique,
+    login                  varchar(32) not null unique,
+    password               varchar(64) not null,
+    created_at             timestamptz not null default now(),
+    credentials_updated_at timestamptz not null default now()
 )
--- src/main/resources/db/changelog/changeset/V001.sql
+++ src/main/resources/db/changelog/changeset/V001.sql
@@ -7,7 +7,3 @@ create table if not exists token
     status        varchar(32) not null default 'ACTIVE',
     user_id       bigint      not null references "user" (id)
 );
-
-alter table "user"
-    add column created_at             timestamptz not null default now(),
-    add column credentials_updated_at timestamptz not null default now();
