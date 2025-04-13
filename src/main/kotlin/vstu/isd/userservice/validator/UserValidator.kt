836c34d16f8925b50323f55129bdce6d17327b57
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-13 16:25:07 +0300

    feat: added validation for 'FindUserRequestDto'

--- src/main/kotlin/vstu/isd/userservice/validator/UserValidator.kt
+++ src/main/kotlin/vstu/isd/userservice/validator/UserValidator.kt
@@ -4,6 +4,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.stereotype.Component
 import vstu.isd.userservice.config.properties.UserValidationRuleProperties
 import vstu.isd.userservice.dto.CreateUserRequestDto
+import vstu.isd.userservice.dto.FindUserRequestDto
 import vstu.isd.userservice.exception.ClientExceptionName
 import vstu.isd.userservice.exception.GroupValidationException
 import vstu.isd.userservice.exception.ValidationException
@@ -20,6 +21,33 @@ class UserValidator(
 
         validateLogin(createUserRequest.login).ifPresent { exceptions.add(it) }
         validatePassword(createUserRequest.password).ifPresent { exceptions.add(it) }
+
+        return if (exceptions.isEmpty()) {
+            Optional.empty()
+        } else Optional.of(
+            GroupValidationException(exceptions)
+        )
+    }
+
+    fun validateFindUserRequest(findUserRequest: FindUserRequestDto): Optional<GroupValidationException> {
+        val exceptions = mutableListOf<ValidationException>()
+
+        if (findUserRequest.id == null && findUserRequest.login == null) {
+            exceptions.add(
+                ValidationException(
+                    "Find request is invalid. All fields are empty.",
+                    ClientExceptionName.INVALID_FIND_USER_REQUEST
+                )
+            )
+
+            Optional.of(
+                GroupValidationException(exceptions)
+            )
+        }
+
+        if (findUserRequest.login != null) {
+            validateLogin(findUserRequest.login).ifPresent { exceptions.add(it) }
+        }
 
         return if (exceptions.isEmpty()) {
             Optional.empty()
