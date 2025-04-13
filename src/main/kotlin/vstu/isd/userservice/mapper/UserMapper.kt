c37cedd8e0697b41bd74cf52252d2e067cf5f9f5
Yurasov Roman <roman.yurasov.v@gmail.com>
2025-04-13 16:23:57 +0300

    feat: added map method from 'FindUserRequestDto' to 'User'

--- src/main/kotlin/vstu/isd/userservice/mapper/UserMapper.kt
+++ src/main/kotlin/vstu/isd/userservice/mapper/UserMapper.kt
@@ -1,6 +1,7 @@
 package vstu.isd.userservice.mapper
 
 import vstu.isd.userservice.dto.CreateUserRequestDto
+import vstu.isd.userservice.dto.FindUserRequestDto
 import vstu.isd.userservice.dto.UserCredentials
 import vstu.isd.userservice.dto.UserDto
 import vstu.isd.userservice.entity.User
@@ -11,3 +12,5 @@ fun CreateUserRequestDto.toEntity() = User(null, login, password)
 
 fun User.toUserCredentials() =
     UserCredentials(id!!, login!!, password!!, credentialsUpdatedAt)
+
+fun FindUserRequestDto.toEntity() = User(id, login, null)
